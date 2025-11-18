789139c708742b21d8dc6cfbf9c0f13058c21433
Pritam Singh <pritam.singh@savein.money>
2025-11-18 14:50:00 +0530

    update

--- js/main.js
+++ js/main.js
@@ -3,16 +3,16 @@ var viewportHeight = window.innerHeight;
 var viewportWidth = window.innerWidth;
 
 var colorArray = [
-  '#D53939',
-  '#90D26D',
-  '#FFEA85',
-  '#DBE2EF',
-  '#2A363B',
-  '#53354A'
+  "#D53939",
+  "#90D26D",
+  "#FFEA85",
+  "#DBE2EF",
+  "#2A363B",
+  "#53354A",
 ];
 
 function setBannerViewPort() {
-  $('.vetti-scene').height(window.innerHeight);
+  $(".vetti-scene").height(window.innerHeight);
 }
 
 function getRandom(min, max) {
@@ -22,56 +22,52 @@ function getRandom(min, max) {
 function randomCircle() {
   if (circleArray.length > 3) {
     removableNode = circleArray.splice(0, 1)[0];
-    $('#' + removableNode).remove();
+    $("#" + removableNode).remove();
   }
-  var id =
-    (+new Date()).toString(36) +
-    Math.random()
-      .toString(36)
-      .slice(2, 7);
+  var id = (+new Date()).toString(36) + Math.random().toString(36).slice(2, 7);
   circleArray.push(id);
-  $('.vetti-scene').append(
+  $(".vetti-scene").append(
     '<div class="rangoli-vattam" id="' + id + '"></div>'
   );
-  window.setTimeout(function() {
+  window.setTimeout(function () {
     const posX = getRandom(0, viewportWidth);
     const posY = getRandom(0, viewportHeight);
-    $('#' + id).css({
-      left: posX + 'px',
-      top: posY + 'px',
+    $("#" + id).css({
+      left: posX + "px",
+      top: posY + "px",
       height: viewportHeight * 2,
       width: viewportHeight * 2,
-      'background-color': colorArray[getRandom(0, colorArray.length - 1)],
-      transform: 'scale3d(3, 3, 3)'
+      "background-color": colorArray[getRandom(0, colorArray.length - 1)],
+      transform: "scale3d(3, 3, 3)",
     });
   }, 100);
 
   window.setTimeout(randomCircle, 10000);
 }
-$(window).on('load', function() {
+$(window).on("load", function () {
   setBannerViewPort();
   $(window).resize(setBannerViewPort);
-  $('.percentage-container').addClass('no-width');
-  setTimeout(function() {
+  $(".percentage-container").addClass("no-width");
+  setTimeout(function () {
     window.clearInterval(id);
-    $('.peru').text('Pritam Parker');
-    $('.seira-vela').text('Javascript Deveoper');
-    $('body').css('overflow', 'auto');
-    $('.rendu').css('display', 'block');
-    $('.down').css('opacity', 1);
+    $(".peru").text("Pritam Singh");
+    $(".seira-vela").text("Frontend Engineering Leader");
+    $("body").css("overflow", "auto");
+    $(".rendu").css("display", "block");
+    $(".down").css("opacity", 1);
     window.setTimeout(randomCircle, 1000);
   }, 1500);
 
-  $(document).scroll(function() {
-    var imgOffset = $('.enaku-therunjathu>h2').offset().top;
+  $(document).scroll(function () {
+    var imgOffset = $(".enaku-therunjathu>h2").offset().top;
     var scroll = $(document).scrollTop();
-    var hasClass = $('.percentage-container').hasClass('no-width');
+    var hasClass = $(".percentage-container").hasClass("no-width");
     if (imgOffset < scroll + (window.innerWidth > 1200 ? 350 : 150)) {
-      $('.percentage-container').removeClass('no-width');
+      $(".percentage-container").removeClass("no-width");
     }
   });
 
   if (window.navigator.platform.match(/win/gi)) {
-    $('body').addClass('windows');
+    $("body").addClass("windows");
   }
 });
